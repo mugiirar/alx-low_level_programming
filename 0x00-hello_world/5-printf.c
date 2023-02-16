5947a38f90709f2f93c024b65400526f8f926f18
mugiirar <munenevincentmugiira@gmail.com>
2023-02-16 08:42:43 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Printing a line with print f.
+ * main - Printing with printf
  * Return: 0
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
-
+	return (0);
+}
