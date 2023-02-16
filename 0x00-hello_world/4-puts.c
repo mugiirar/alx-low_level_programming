e8a5dad733d244ad6929315fe00baf1ddf25c628
mugiirar <munenevincentmugiira@gmail.com>
2023-02-16 07:00:47 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main -Writing a c programme prints a line.
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
