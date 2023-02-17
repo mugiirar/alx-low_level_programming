a2e7f0754d34f54756ef6013965d524130433fd1
mugiirar <munenevincentmugiira@gmail.com>
2023-02-17 06:53:34 -0800

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - alphabetic
+ * Return: Always
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
