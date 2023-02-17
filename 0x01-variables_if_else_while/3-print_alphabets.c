0e2e329c291ff5fba7b8e0908c78148aa7ca9ff1
mugiirar <munenevincentmugiira@gmail.com>
2023-02-17 06:20:53 -0800

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'z'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
