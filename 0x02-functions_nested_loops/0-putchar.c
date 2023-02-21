54ec7304f87f38b497dae820ce0e0878e3019e69
mugiirar <munenevincentmugiira@gmail.com>
2023-02-21 04:54:26 -0800

    adding putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- * main - Printing a word putchar on thy screen
- *
- *
- * Return = 0.  This means that it was printed fine.
- * On error a message is prompted
+ * main - Printing char
+ * Return: Always 0
  */
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
