e77adaeb6a689bd5bddc3f2d54e56bf63a0eded5
mugiirar <munenevincentmugiira@gmail.com>
2023-02-21 09:01:29 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * _islower - checking for upper and lower case
+ * Return: 1 for lowercase 0 for upper
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
