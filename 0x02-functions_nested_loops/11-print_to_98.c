3574d3ab84fb7a482bbaeca9a68ec73585136c6e
mugiirar <munenevincentmugiira@gmail.com>
2023-02-21 16:58:42 -0800

    100 times

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print n to 98 counts
+ * separated by comma, followed
+ * by space and number should be
+ * printed in order
+ *
+ * @n: input
+*/
+
+int print_to_98(int n)
+{
+	if (n <= 98)
+	{
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n>= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,3 +21,5 @@ int add(int, int);
 void print_to_98(int n);
 
 int _putchar(char c);
+
+void print_times_table(int n);
