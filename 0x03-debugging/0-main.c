0541a41968686a089b5c7f46144df9c61e33fb04
mugiirar <munenevincentmugiira@gmail.com>
2023-02-22 06:33:55 -0800

    maiin

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
-void positive_or_negative(int q)
+/**
+ * main - Determines whether a number is positive or negative
+ * @i: a given input
+ * Return: Always 0.
+ */
+int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
