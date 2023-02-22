8612e62ea38252a21480a43d3bd98231cd949854
mugiirar <munenevincentmugiira@gmail.com>
2023-02-21 17:22:09 -0800

    nature

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of multiples of 3 0r 5 to 1024
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int p, d = 0;
+
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	z += i;
+	}
+	i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
