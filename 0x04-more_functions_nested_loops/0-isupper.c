c5d399c9ef547670f2e8439e0c3432d126bc2430
mugiirar <munenevincentmugiira@gmail.com>
2023-02-22 23:38:55 -0800

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - determines if the character is uppercase.
+ * @c: input value
+ * Return: 1 for uppercase or 0 for lowercase
+ */
+
+int _isupper(int c)
+{
+
+	if (isupper(c))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
