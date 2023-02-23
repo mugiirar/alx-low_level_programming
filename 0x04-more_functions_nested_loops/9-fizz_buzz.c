8990e8e400666b9c39d286b6b83a48e0da145098
mugiirar <munenevincentmugiira@gmail.com>
2023-02-23 05:07:33 -0800

    pp

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,19 @@ int main(void)
 	{
 	if (n % 3 == 0 && n % 5 == 0)
 	{
-	printf("FizzBuzz\n");
+	printf(" FizzBuzz");
 	}
 	else if (n % 3 == 0)
 	{
-		printf("Fizz\n");
+		printf(" Fizz");
 	}
 	else if (n % 5 == 0)
 	{
-		printf("Buzz\n");
+		printf(" Buzz");
 	}
 	else
 	{
-		printf("%d\n", n);
+		printf(" %d", n);
 	}
 	}
 	putchar('\n');
