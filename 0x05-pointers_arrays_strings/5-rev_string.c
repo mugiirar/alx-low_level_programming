e25b4afc50f072aa6fe076f65d040a80bbb9935d
mugiirar <munenevincentmugiira@gmail.com>
2023-02-27 04:13:37 -0800

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,12 +8,16 @@
 
 void rev_string(char *s)
 {
-	int len = strlen(s);
-	int i;
+	int len = 0, index = 0;
+	char tmp;
 
-	for (i = len - 1; i >= 0; i--)
+	while (s[index++])
+	len++;
+
+	for (index = len -1; index >= len / 2; index--)
 	{
-		printf("%c", s[i]);
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
 	}
-	printf("\n");
 }
