ead1cb024306f1dfb796db4ba22e174b13a00b1d
mugiirar <munenevincentmugiira@gmail.com>
2023-03-01 12:05:22 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,43 +4,27 @@
 /**
  * leet - a function that encodes a string into 1337
  *
- * @s: string input
+ * @str: string input
  *
- * Return: @s
+ * Return: @str
 */
 
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i, j;
-	char *result = (char *) malloc(strlen(s) + 1);
+	char ch[] = "aAeEoOtlL";
+	char leet[] = "4433007711";
+	int x, y;
 
-	for (i = 0, j = 0; s[i] != '\0'; i++, j++)
+	for (x = 0; str[x] != '\0'; x++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		for (y = 0; ch[y] != '\0'; y++)
 		{
-			result[j] = '4';
-		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			result[j] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			result[j] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			result[j] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			result[j] = '1';
-		}
-		else
-		{
-			result[j] = s[i];
+			if (str[x] == ch[y])
+			{
+				str[x] = leet[y];
+				break;
+			}
 		}
 	}
-	result[j] = '\0';
-	return (result);
+	return (str);
 }
