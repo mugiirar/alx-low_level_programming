e9d91b7e3bd90f40c843f48a387ad2db7ea0cb95
mugiirar <munenevincentmugiira@gmail.com>
2023-03-06 00:55:52 -0800

    string break

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strstr - finds the first occurrence of the substring needle
+ * in the string haystack
+ * @s: char input
+ * @accept: char input
+ * Return: char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return strpbrk(s, accept);
+}
