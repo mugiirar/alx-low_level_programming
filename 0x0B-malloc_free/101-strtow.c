fe28f5ec911187126ffb6bcb73ed73b0994c74bb
mugiirar <munenevincentmugiira@gmail.com>
2023-03-13 23:04:09 -0700

    strtow

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * count_word - helper function to count the number
+ * @s: string
+ *
+ * Return: words
+ */
+
+int count_word(char *s)
+{
+	int g, p, w;
+
+	g = 0;
+	w = 0;
+
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		if (s[p] == ' ')
+			g = 0;
+		else if (g == 0)
+		{
+			g = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **m, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	m = (char **) malloc(sizeof(char *) * (words + 1));
+	if (m == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				m[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	m[k] = NULL;
+
+	return (m);
+}
