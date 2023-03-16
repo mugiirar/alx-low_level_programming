b26e7561b232b37defa1fcebc29e67de46d97843
mugiirar <munenevincentmugiira@gmail.com>
2023-03-16 09:31:11 -0700

    2 - calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,11 +1,32 @@
 #include "main.h"
+#include <stdlib.h>
+
 
 /**
- * *_calloc - a function that allocates memory for an array, using
- * malloc
- * @nmemb: integer input
- * @size: integer input
- * return: nothing
+ * *_tozero - fills memory block
+ * @s: pointer input
+ * @a: character input
+ * @zas: integer iput
+ * Return: char
+ */
+
+char *_tozero(char *s, char a, unsigned int zas)
+{
+	unsigned int k = 0;
+
+	while (k < zas)
+	{
+		s[k] = a;
+		k++;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - function that allocates memory for an array
+ * @nmemb: int input
+ * @size: int input
+ * Return: 0
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -21,6 +42,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
+
+	_tozero(p, 0, nmemb * size);
 	return (p);
 }
 
