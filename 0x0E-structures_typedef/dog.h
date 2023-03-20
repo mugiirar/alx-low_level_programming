fb089dcfec5063866eeaea89953c839b0b90587d
mugiirar <munenevincentmugiira@gmail.com>
2023-03-20 02:40:23 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
 #include <stdio.h>
 #include <string.h>
-
-typedef struct dog{
+/**
+ * struct dog - doggy information
+ * @name: char input
+ * @age: float input
+ * @owner: char input
+ * dog_t - instance of struct dog
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 #include "main.h"
+
+#endif
