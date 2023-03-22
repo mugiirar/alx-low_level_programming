269c957d232ea04756629a7ef9890a2a39d45fdd
mugiirar <munenevincentmugiira@gmail.com>
2023-03-22 04:27:28 -0700

    iteratoor

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,13 +14,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int p;
 
-	if (!array || !action)
+	if (array != NULL && action != NULL && size > 0)
 	{
-		return;
+		for (p = 0; p < size; p++)
+		{
+			action(array[p]);
+		}
 	}
-
-	for (p = 0; p <= size; p++)
-	{
-		action(array[p]);
-	}
+	
 }
