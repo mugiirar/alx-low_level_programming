aa8928f1f9b82cb716310ff499f858793c0c4c65
mugiirar <munenevincentmugiira@gmail.com>
2023-04-03 12:58:53 -0700

    printing elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,13 +12,22 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t p = 0;
+	size_t node_count = 1;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
-		p++;
+
 		h = h->next;
+		node_count++;
 	}
-	return (p);
+
+	/*print last node*/
+	printf("%d\n", h->n);
+
+	return (node_count);
 }
+
