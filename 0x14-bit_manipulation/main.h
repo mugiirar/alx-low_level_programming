ecde1d3131f6458a18d4a3400bf31536575a1231
mugiirar <munenevincentmugiira@gmail.com>
2023-04-06 06:22:09 -0700

    endianness

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -13,4 +13,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 #endif
