5d11e6c7ec43879ffc6e357eef181d961e5f8f0f
mugiirar <munenevincentmugiira@gmail.com>
2023-04-08 13:12:57 -0700

    creating fil

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,16 +23,16 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	len = 0;
-	while (text_content[len])
-		len++;
-
 	if (text_content != NULL)
 	{
+	while (text_content[len])
+		len++;
+	}
+
 	y = write(fp, text_content, len);
 
 	if (y < 0)
 		return (-1);
-	}
 
 	close(fp);
 	return (1);
