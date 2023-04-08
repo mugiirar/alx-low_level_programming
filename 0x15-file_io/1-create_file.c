0c62cf9567ceb8be0f9789f9c0fbeb025a6efd10
mugiirar <munenevincentmugiira@gmail.com>
2023-04-08 13:05:20 -0700

    creating file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file.
+ * @filename: name of new file
+ * @text_content: info to be written
+ * Return: 1 for success or -1 for faliure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+	int len, y;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fp < 0)
+	{
+		return (-1);
+	}
+	len = 0;
+	while (text_content[len])
+		len++;
+
+	if (text_content != NULL)
+	{
+	y = write(fp, text_content, len);
+
+	if (y < 0)
+		return (-1);
+	}
+
+	close(fp);
+	return (1);
+}
