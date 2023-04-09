ad04645cd8bb7466e2f64000a93227845097458e
mugiirar <munenevincentmugiira@gmail.com>
2023-04-09 06:36:26 -0700

    coppying files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,10 +11,7 @@ int main(int argc, const char *argv[])
 {
 	int fp_f;
 	int fp_t;
-	int n;
-	int m;
-	int x;
-	int y;
+	int n, m, x, y;
 	char *buffer[BUFF_SIZE];
 
 	if (argc != 3)
@@ -22,28 +19,23 @@ int main(int argc, const char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fp_f = open(argv[1], O_RDONLY);
-
 	if (fp_f < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fp_t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-
 	while ((n = read(fp_f, buffer, BUFF_SIZE)) > 0)
 	{
-		if ((m = write(fp_t, buffer, n)) != n || fp_t < 0)
+		m = write(fp_t, buffer, n);
+		if (m != n)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fp_t);
-			exit (99);
+			exit(99);
 		}
 	}
-	
 	x = close(fp_f);
 	if (x < 0)
 	{
@@ -56,8 +48,5 @@ int main(int argc, const char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_t);
 		exit(100);
 	}
-
 	return (0);
 }
-
-
