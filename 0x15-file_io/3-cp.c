d6442913587626778f07147187889f03b292965e
mugiirar <munenevincentmugiira@gmail.com>
2023-05-08 11:44:56 -0700

    debug cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,42 +1,50 @@
 #include "main.h"
-
-int main(int argc, char *argv[])
+#define BUFF_SIZE 1024
+/**
+ * main - a program that copies the content of a file to another file
+ * @argc: number of args
+ * @argv: array of arguments
+ * Return: 0 for success or -1 for faliure
+ */
+int main(int argc, const char *argv[])
 {
-	int fp_f, fp_t, w, k, v;
-	char *buff[1024];
+	int fp_f, fp_t, n, m, x, y;
+	char *buffer[BUFF_SIZE];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fp_f = open(argv[1], O_RDONLY);
 	if (fp_f < 0)
 	{
-		close(fp_f);
-		dprintf(STDERR_FILENO, " Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fp_t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((read(fp_f, buff, 1024) > 0))
+	while ((n = read(fp_f, buffer, BUFF_SIZE)) > 0)
 	{
-		w = write(fp_t, buff, 1024);
-		if (w < 0)
+		m = write(fp_t, buffer, n);
+		if (m != n || fp_t < 0)
 		{
-			close(fp_t);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	k = close(fp_t);
-	v = close(fp_f);
-	if (k < 0 || v < 0)
+	if (n < 0)
 	{
-		if (k < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d", fp_t);
-		if (v < 0)
-			dprintf(STDERR_FILENO, " Error: Can't close fd %d", fp_f);
-
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	x = close(fp_f);
+	y = close(fp_t);
+	if (y < 0 || x < 0)
+	{
+		if (y < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_t);
+		if (x < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_f);
 		exit(100);
 	}
 	return (0);
