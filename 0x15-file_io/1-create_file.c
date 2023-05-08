800c2e4423ef74af13e90042fe5cb97f8b4d0794
mugiirar <munenevincentmugiira@gmail.com>
2023-05-08 01:10:12 -0700

    Creating and writing a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp, k, p;
+
+
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (filename == NULL)
+	{
+		close(fp);
+		return(-1);
+	}
+
+	if (text_content == NULL)
+	{
+		close (fp);
+		return(1);
+	}
+
+	k = strlen(text_content);
+
+	p = write(fp, text_content, k);
+
+	if (fp < 0 || p < 0)
+	{
+		close (fp);
+		return (-1);
+	}
+	close(fp);
+	return (1);
+}
