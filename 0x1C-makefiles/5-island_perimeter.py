c7cd2cbdd016e23b85c9438dc9b2f0c340de635c
mugiirar <munenevincentmugiira@gmail.com>
2023-10-25 10:41:56 -0700

    iceland

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -18,16 +18,16 @@ def island_perimeter(grid):
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
-                # Count the top edge
+                
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                # Count the bottom edge
+                
                 if i == height - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                # Count the left edge
+                
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                # Count the right edge
+                
                 if j == width - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
     
